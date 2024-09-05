70dd6be311191c6672b12645ae7b35a07f64a2ff
Bao Ngo <ngobao2001@gmail.com>
2024-09-05 18:42:24 +0800

    Fix path

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=["get", "post"])
+@app.route('/', methods=["get"])
 def index():
   return render_template('index.html')
 
-@app.route('/active_offers.html', methods=["get", "post"])
+@app.route('/active_offers', methods=["get"])
 def active_offers():
   return render_template('active_offers.html')
 
-@app.route('/create_offer', methods=["get", "post"])
+@app.route('/create_offer', methods=["get"])
 def create_offer():
   return render_template('create_offer.html')
 
